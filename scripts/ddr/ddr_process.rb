ebf91b04ce4fa2ff8bc95799152454a57e94b1ab
Lars Povlsen <lars.povlsen@microchip.com>
2023-01-23 11:24:30 +0100

    gen_cfg.rb: Fixes from JSA wrt post_cke_x1024, tdinit0, tdinit2

--- scripts/ddr/ddr_process.rb
+++ scripts/ddr/ddr_process.rb
@@ -1,3 +1,7 @@
+def max (a,b)
+  a>b ? a : b
+end
+
 def ddr_process(params)
     ddr_clk_ns = (1000.0 / (params[:clock_speed] / 4.0)).round(2)
 
@@ -7,22 +11,22 @@ def ddr_process(params)
     params[:phy_clk_period] = ddr_clk_ns
     params[:ctl_clk_period] = ddr_clk_ns
 
-    ## For DDR4, dfi_unit = 1638 ns
-    ## For DDR3, dfi_unit = 2048 ns
-    params[:dfi_unit] = (1024 * params[:ctl_clk_period]).ceil()
+    ## dfi_unit is 1024 dfi clocks
+    params[:dfi_unit] = (1024 * params[:dfi_clk_period]).ceil()
     $l.debug "params(dfi_unit) = #{params[:dfi_unit]} ns"
 
-    params[:reset_period] = 200000; ## 200 us
-    params[:init_period]  = 500000; ## 500 us
-    params[:wait_reset] = 12
-    params[:wait_init]  = 30
+    params[:reset_period] = 200000; ## 200 us (jsa: tPW_RESET_L)
+    params[:init_period]  = 500000; ## 500 us (jsa: OK)
+    params[:wait_reset] = 12;       ## (jsa: Unused)
+    params[:wait_init]  = 30;       ## (jsa: Unused)
 
     # Validation / reference board
     params[:pre_cke_x1024] =   (2.0 * (params[:init_period] / params[:dfi_unit])).ceil()
-    params[:post_cke_x1024] =  (2.0 * (params[:tXPR] / params[:dfi_unit])).ceil()
+    params[:post_cke_x1024] =  max(2,(2.0 * (params[:tXPR] / (1000*params[:dfi_unit]))).ceil());  ## tXPR is ps, dfi_unit is ns !
+    #$l.debug "params(tXPR) = #{params[:tXPR]} ps";  ## /jsa
     params[:dram_rstn_x1024] = (2.0 * (params[:reset_period] / params[:dfi_unit])).ceil()
-    params[:tdinit0] = params[:pre_cke_x1024]
-    params[:tdinit2] = params[:dram_rstn_x1024]
+    params[:tdinit0] = 1024*params[:pre_cke_x1024];   ## tdinit0 is in tCK
+    params[:tdinit2] = 1024*params[:dram_rstn_x1024]; ## tdinit2 is in tCK
     params[:tpllgs] =   (4000.0 / params[:ctl_clk_period]).ceil()
     params[:tpllpd] =   (1000.0 / params[:ctl_clk_period]).ceil()
     params[:tplllock] = (100000 / params[:ctl_clk_period]).ceil()
