7d5f3c73c07384cfab53210bb51c0adfb2f06056
Lars Povlsen <lars.povlsen@microchip.com>
2023-02-20 09:54:59 +0100

    chip: Fix ddr3/ddr4 register issue

--- src/pcb135_config.c
+++ src/pcb135_config.c
@@ -8,7 +8,7 @@
 
 const struct ddr_config pcb135_ddr3_ddr_config = {
 	.info = {
-		.name = "pcb135_ddr3 2023-02-20-08:48:52 735ea6656433-dirty",
+		.name = "pcb135_ddr3 2023-02-20-08:54:14 2b55950de23b-dirty",
 		.speed = 1667,
 		.size = 0x80000000,
 		.bus_width = 32,
