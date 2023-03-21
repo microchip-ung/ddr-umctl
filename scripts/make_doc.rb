5d49969264c5d9ac85ac3c8581f76bc519c79cf3
Lars Povlsen <lars.povlsen@microchip.com>
2023-03-21 15:59:54 +0100

    doc: Add register and field documentation

--- scripts/make_doc.rb
+++ scripts/make_doc.rb
@@ -7,6 +7,37 @@ require 'fileutils'
 
 require_relative 'soc/chip.rb'
 
+def make_row(fields, fname)
+    doc = Array.new
+    fields.keys.each do |p|
+        if fields[p]
+            f = fields[p].find{|h| h[:name] == fname}
+            if f
+                doc << f[:doc].gsub("\n", " ")
+            else
+                doc << ""
+            end
+        else
+            doc << ""
+        end
+    end
+
+    str = ""
+    while doc.length > 0
+        if doc.length >= 3 && doc[0] == doc[1] && doc[1] == doc[2]
+            str += (" 3+| " + doc[0])
+            doc.shift(3)
+        elsif doc.length >= 2 && doc[0] == doc[1]
+            str += (" 2+| " + doc[0])
+            doc.shift(2)
+        else
+            str += (" | " + doc.shift)
+        end
+    end
+
+    return str
+end
+
 soc = Hash.new
 cfg = Hash.new
 %w(lan966x lan969x sparx5).each do|p|
