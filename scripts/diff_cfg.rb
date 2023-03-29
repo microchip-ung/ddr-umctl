da56fe3d5ee88b7caaaa6944a1883adf48d0be0f
Lars Povlsen <lars.povlsen@microchip.com>
2023-03-29 10:37:26 +0200

    diff_cfg.rb: Allow input from stdin ('-' for one of the files)

--- scripts/diff_cfg.rb
+++ scripts/diff_cfg.rb
@@ -61,9 +61,9 @@ if ARGV.length != 2
 end
 
 f1 = ARGV.shift
-cfg1 = YAML::load_file(f1)
+cfg1 = f1 == "-" ? YAML::load(STDIN.read) : YAML::load_file(f1)
 f2 = ARGV.shift
-cfg2 = YAML::load_file(f2)
+cfg2 = f2 == "-" ? YAML::load(STDIN.read) : YAML::load_file(f2)
 
 # Prepare default values, registers, etc.
 platform = $option[:platform] ? $option[:platform] : cfg1['info']['platform']
